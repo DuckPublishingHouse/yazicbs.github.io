697719bff1495847dbf2d103c4e42a383d23f8ee
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-08-15 16:25:00 +0800

    直接爆了
    避免This step has been truncated due to its large size. Download the full logs from the menu once the workflow run has completed.

--- .action_scripts/check-local-paths.js
+++ .action_scripts/check-local-paths.js
@@ -2,16 +2,18 @@ const fs = require('fs');
 const glob = require('glob');
 
 // 修改正则表达式，以匹配绝对路径或包含驱动器的路径
-const pattern = /(?:[A-Z]:\\|\/[^\/:*?"<>|\r\n][^\\/:*?"<>|\r\n]*[\w\/\\][^\\/:*?"<>|\r\n]*|(?:\/[^\/:*?"<>|\r\n]+)+[^\\/:*?"<>|\r\n])/g;
+const pattern = /(?:[A-Z]:\\)/g;
 const files = ['**/*.html', '**/*.js', '**/*.css'];
+
+let foundPath = false; // 用于跟踪是否已经找到路径
 
 files.forEach(globPattern => {
     const filePaths = glob.sync(globPattern, { nodir: true });
     filePaths.forEach(filePath => {
+        if (foundPath) return; // 如果已经找到路径，则跳过进一步检查
+
         const content = fs.readFileSync(filePath, 'utf8');
         const lines = content.split('\n');
-        
-        let foundPath = false;
         
         lines.forEach((line, lineNumber) => {
             let match;
@@ -21,6 +23,7 @@ files.forEach(globPattern => {
                     // 打印检测到的路径及其行号和列号
                     console.error(`✕ 在 ${filePath} 第 ${lineNumber + 1} 行第 ${match.index + 1} 列检测到本地路径: ${match[0]}`);
                     foundPath = true;
+                    process.exit(1); // 找到路径后退出程序
                 }
             }
         });

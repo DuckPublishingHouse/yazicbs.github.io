8d91f7501774c15389b158db5a97fa6a0002faf1
鸭鸭「カモ」 <Yzcbs123@163.com>
2024-10-15 23:14:12 +0800

    fix 错误的函数调用

--- zh_cn/js/games/guess_number.js
+++ zh_cn/js/games/guess_number.js
@@ -40,7 +40,7 @@ function Guess_number(n) {
       // MDN Docs: https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Global_Objects/Number/isInteger
       //
       // 用 console.log/warn/error 不好吗? 为什么在不需要返回的时候硬要 return
-      gm_init(guess_max, guess_min);
+      Guess_number_init(guess_max, guess_min);
       console.log("[Game\\Guess Number] 恭喜, 您猜对了！\n[Game\\Guess Number] 已自动初始化下一局游戏, 如需修改范围请重新使用 gm_init(最小数, 最大数) 初始化");
     }
   } else {
